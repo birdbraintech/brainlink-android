f2daf72950e7b90d7067bdf8550424800c8ab64e
tlauwers@gmail.com <tlauwers@gmail.com@fea9841e-259b-9378-3358-b5c41f025ad5>
2011-12-05 17:41:46 +0000

    Added the API activity

---
+++ trunk/brainlink-android/BrainLinkAndroidAPI/src/edu/cmu/ri/createlab/BrainLinkAndroidAPIActivity.java
@@ -0,0 +1,13 @@
+package edu.cmu.ri.createlab;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class BrainLinkAndroidAPIActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
